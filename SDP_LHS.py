52e71c994031271b236a77ce48a49d5dd2fc35ab
BiduRuiz <87830749+BiduRuiz@users.noreply.github.com>
2022-04-28 14:34:06 -0300

    Redefined variables in PICOS

--- SDP_LHS.py
+++ SDP_LHS.py
@@ -1,20 +1,29 @@
 import numpy as np
-import picos as pc
+import picos as pic
 
-def SDP_LHS(k,m,rho,rho_sep,eta,estrategias,medicoes,identidade):
+def SDP_LHS(k_out,m_mea,rho_ent,rho_loc,eta_in,estrategias,medicoes):
 
-    #Estado de 2 qubits
-    qub = 4
-    #Identidade dim = 2
-    iden_2 = np.array([[1,0],[0,1]]) 
-    
+
     #Criando o SDP
-    prob = pc.Problem()
+    prob = pic.Problem()
+
+    #Definimos agora todas as variáveis e constantes dentro do PICOS
+    #Constantes:
+    k = pic.Constant('k',k_out)
+    m = pic.Constant('m',m_mea)
+    rho = pic.Constant('rho',rho_ent)
+    rho_sep = pic.Constant('rho_sep',rho_loc)
+    eta = pic.Constant('eta',eta_in)
+    est = pic.Constant('est_det',estrategias)
+    mea = [pic.Constant('meas[{}]'.format(i),medicoes[i])for i in range(k*m)]
+    #Matrix identidade 2x2:
+    iMat = pic.Constant('I', np.eye(2,dtype='complex'))
     
-    #Definindo as variáveis que queremos encontrar
-    q = pc.RealVariable('q',1)
-    sigma = [prob.add_variable("sigma[{}]".format(i), (2, 2)) for i in range(k**m)]
-    chi = pc.HermitianVariable('chi',qub)
+    #Variáveis
+    q = pic.RealVariable('q')
+    chi = pic.HermitianVariable('chi',(4,4))
+    #Definindo o amsenble {Sigma}_lambda que queremos encontrar
+    sigma = [prob.add_variable('sigma[{}]'.format(i), (2, 2), 'hermitian') for i in range(k**m)]
     
     #Objetivo do SDP: q* = max(q)
     prob.set_objective('max',q)
@@ -22,14 +31,14 @@ def SDP_LHS(k,m,rho,rho_sep,eta,estrategias,medicoes,identidade):
     #Definindo as restrições das variáveis
     prob.add_constraint(q>=0)
     prob.add_constraint(q<=1)
-    prob.add_list_of_constraints([sigma[i]>=0 for i in range(k**m)],'i','0...k**m')
+    prob.add_list_of_constraints([sigma[i]>>0 for i in range(k**m)])
     
     #Definindo as outras restrições
-    prob.add_constraint(q*rho+(1-q)*rho_sep == eta*chi+(1-eta)*pc.partial_trace(rho_sep,dim=4)@pc.partial_trace(chi, dim=4))
-    prob.add_list_of_constraints([pc.partial_trace(np.kron(medicoes[i],iden_2)*chi, dim=4)==pc.sum([estrategias[j][i]*sigma[j]for j in range(k**m)]) for i in range(k*m)],'i','0...k*m')
+    prob.add_constraint(q*rho+(1-q)*rho_sep == eta*chi+(1-eta)*iMat@(pic.partial_trace(chi,subsystems=(1),dimensions=(2,2))))
+    prob.add_list_of_constraints([pic.partial_trace(mea[i]@(pic.partial_trace(rho_sep,subsystems=(1),dimensions=(2,2)))*(chi),subsystems=(0),dimensions=(2,2))==pic.sum([estrategias[j][i]*sigma[j]for j in range(k**m)]) for i in range(k*m)])
 
     #Resolvendo o SDP
-    prob.options.solver = 'gurobi'
+    prob.options.solver = 'mosek'
     solution = prob.solve()
     
-    return sigma, q, chi, solution, prob
+    return sigma, q, chi, solution, prob, k, m, rho, rho_sep, eta, est, mea
